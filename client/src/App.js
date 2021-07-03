f9d916f38a17dfe264c2975bb385730e7c54ff38
noah figueras <noahfigueras@gmail.com>
2021-07-03 00:25:21 -0700

    App Successfully connected with Web3

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
 
-import Lottery from '../artifacts/Lottery.json';
+import Lottery from './artifacts/Lottery.json';
 
-//localhost
-const lotteryAddress = '';
+//Kovan Network
+const lotteryAddress = '0x49c21F64501ffEF20eEeA30b40387F8A3F81a64C';
 
 //Move this to context
 const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -17,7 +17,7 @@ async function requestAccount() {
         await window.ethereum.request({method: 'eth_requestAccounts'});
     } catch(err) {
         console.log('error');
-        console.error(error);
+        console.error(err);
 
         alert('Login to Metamask first');
     }
@@ -29,7 +29,8 @@ function App() {
         if(typeof window.ethereum !== 'undefined'){
             try{
                 const data = await contract.price();
-                console.log('Data: ', data);
+                const price  = data.toString();
+                console.log('Data: ', ethers.utils.formatEther(price));
             } catch(err) {
                 console.log('Error: ', err);
             }    
@@ -39,12 +40,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <h1> Blockchain Lottery </h1>
         <p>
           Join the Blockchain Lottery and win millions every week.
         </p>
         <button
-        className="join_lottery" onClick={() => alert("Suck it bitch!")}
+        className="join_lottery" onClick={() => getLotteryPrice()}
         >
           Enter Lottery
         </button>
